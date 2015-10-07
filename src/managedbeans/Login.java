78195c51d6f318345f398da0d56882b55f04fee2
Gustavo Melo <gcastromelo94@gmail.com>
2015-10-07 20:33:20 -0300

    sei la

--- src/managedbeans/Login.java
+++ src/managedbeans/Login.java
@@ -5,7 +5,29 @@ import javax.faces.bean.*;
 @ManagedBean
 public class Login {
 	
+	private String email;
+	private String pass;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
 	public String doLogin() {
+		setEmail("teste");
+		setPass("teste2");
 		return "index";
 	}
+	
 }
